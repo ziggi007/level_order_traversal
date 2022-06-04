79c9153ee87aaa9f295f9332250459a455d1fcf2
ziggi007 <95996000+ziggi007@users.noreply.github.com>
2022-06-05 03:56:51 +0530

    Add files via upload

---
+++ binaryTree.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+int data ;
+Node* left ;
+Node* right ;
+   
+Node(int val){
+  data=val;
+  left=NULL;
+  right=NULL;
+}
+
+};
+
+
+void print (Node * root){
+  cout<<root->data<<" ";
+  return;
+}
+void levelorder(struct Node* root){
+  queue<Node*>q;
+  // vector<vector<int>>ans;
+  q.push(root);
+  while(!q.empty()){
+    
+    Node* curr=q.front();
+    print(curr);
+     q.pop();
+    if(curr->left ){
+      q.push(curr->left);
+    }
+    if(curr->right){
+        q.push(curr->right);
+    }
+    
+   
+
+  }
+
+}
+
+
+int main ()
+{
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+    struct 
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    levelorder(root);
+  
+      return 0;
+  }
+ 
